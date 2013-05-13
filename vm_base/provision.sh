485dca10f34e4bca90793f2537098f60e42eed3e
Leo Lapworth <leo@cuckoo.org>
2013-05-13 22:00:01 +0100

    Try mount var in /tmp/meta_var

--- vm_base/provision.sh
+++ vm_base/provision.sh
@@ -1,2 +1,9 @@
+
+# Break open the var cache here for metacpan.org
+mkdir -p /tmp/meta_var/tmp
+chmod -R 777 /tmp/meta_var
+ln -s /tmp/meta_var /home/metacpan/metacpan.org/var
+
+# start up the services
 for service in elasticsearch metacpan-api metacpan-www;
   { service $service start; }
