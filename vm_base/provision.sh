a9338e5f01c447fc0dc33b6e9d781b5400c9dc4e
Leo Lapworth <leo@cuckoo.org>
2013-05-14 21:08:05 +0100

    Get puppet working for vm_base

--- vm_base/provision.sh
+++ vm_base/provision.sh
@@ -1,3 +1,14 @@
+# Sort out puppet's ssl dir so it can be written to
+ssldir=/etc/puppet/ssl
+mkdir -p $ssldir
+
+ssl_bind_src=/etc/puppet-ssl
+mkdir -p $ssl_bind_src
+chown puppet:puppet $ssl_bind_src
+
+mount --bind $ssl_bind_src $ssldir
+
+chown -R puppet:puppet $ssldir
 
 # Break open the var cache here for metacpan.org
 mkdir -p /tmp/meta_var/tmp
